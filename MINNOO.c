37c83b3a82cefcf6da9bc2becb9fc5cadaefebe9
vikasjackar <87568428+vikasjackar@users.noreply.github.com>
2022-12-08 23:20:55 +0530

    MINNOO-codechef

---
+++ MINNOO.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int n;
+	    scanf("%d",&n);
+	    printf("%d\n",(n/2));
+	}
+	return 0;
+}
+
