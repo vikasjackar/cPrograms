ab7fc475b345aa41f17ffa7a98118396d2b64938
vikasjackar <87568428+vikasjackar@users.noreply.github.com>
2022-12-23 00:08:20 +0530

    DIFFSUM-codechef

---
+++ DIFFSUM.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a,b;
+	scanf("%d",&a);
+	
+	scanf("%d",&b);
+	if(a>b)
+	printf("%d",(a-b));
+	else
+	printf("%d",(a+b));
+	return 0;
+}
+
