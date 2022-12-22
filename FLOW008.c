868e83e7b7c0fd23724642047e7da2c3907c7c84
vikasjackar <87568428+vikasjackar@users.noreply.github.com>
2022-12-23 00:10:58 +0530

    FLOW008-codechef

---
+++ FLOW008.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int n;
+	    scanf("%d",&n);
+	    if(n<10)
+	    printf("Thanks for helping Chef!\n");
+	    else
+	    printf("-1\n");
+	}
+	return 0;
+}
+
