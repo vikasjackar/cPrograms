c9aa3f43aeb242a8d941a4445e15e51704148316
vikasjackar <87568428+vikasjackar@users.noreply.github.com>
2022-12-23 00:04:06 +0530

    CHEFPAGES-codechef

---
+++ CHEFPAGES.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a,b;
+	scanf("%d %d",&a,&b);
+	if(a==0)
+	printf("https://www.codechef.com/practice");
+	else if(a==1 && b==0)
+	printf("https://www.codechef.com/contests");
+	else if(a==1 && b==1)
+	printf("https://discuss.codechef.com");
+	return 0;
+}
+
