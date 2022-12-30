8973a663689e0b634dccbeac4a1130fe8b2b029d
vikasjackar <87568428+vikasjackar@users.noreply.github.com>
2022-12-30 22:35:23 +0530

    v-codechef

---
+++ PROBCAT (2).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x;
+	    scanf("%d",&x);
+	    if(x>=1 && x<100)
+	    printf("Easy\n");
+	    else if(x>=100 && x<200)
+	    printf("Medium\n");
+	    else if(x>=200 && x<=300)
+	    printf("Hard\n");
+	}
+	return 0;
+}
+
