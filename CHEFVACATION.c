c2ff81d3f1de2a12c8cb13288778233bf0908dfe
vikasjackar <87568428+vikasjackar@users.noreply.github.com>
2023-01-01 22:54:55 +0530

    CHEFVACATION-codechef

---
+++ CHEFVACATION.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x,y,z;
+	    scanf("%d %d %d",&x,&y,&z);
+	    if((x+y)<=z)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    printf("NO\n");
+	}
+	return 0;
+}
+
