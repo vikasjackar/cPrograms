290e9d37975ef79492ce5e3bf5b7eaf20c9dd5f8
vikasjackar <87568428+vikasjackar@users.noreply.github.com>
2023-01-07 22:52:24 +0530

    APPLORNG-codechef

---
+++ APPLORNG.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int x;
+	scanf("%d",&x);
+	int a,b;
+	scanf("%d %d",&a,&b);
+	if((a+b)<=x)
+	printf("YES\n");
+	else
+	printf("NO\n");
+	return 0;
+}
+
