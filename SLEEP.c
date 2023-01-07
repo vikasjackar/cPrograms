b62aad45f51ac4c0b31898ddefd431a0d7b94d58
vikasjackar <87568428+vikasjackar@users.noreply.github.com>
2023-01-07 22:51:20 +0530

    SLEEP-codechef

---
+++ SLEEP.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x;
+	    scanf("%d",&x);
+	    if(x<7)
+	    printf("YES\n");
+	    else
+	    printf("NO\n");
+	}
+	return 0;
+}
+
