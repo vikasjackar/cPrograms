b861a326588045d922c83df96fd17d27d13b3008
vikasjackar <87568428+vikasjackar@users.noreply.github.com>
2023-02-01 22:54:36 +0530

    CARRANGE-codechef

---
+++ CARRANGE.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x,y,z;
+	    scanf("%d%d%d",&x,&y,&z);
+	  printf("%d\n",(y-(x-1))*z);
+	        
+	    
+	}
+	return 0;
+}
+
