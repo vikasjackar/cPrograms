ee51721bca40289b5d9eab5e871e3f75bd9faf71
vikasjackar <87568428+vikasjackar@users.noreply.github.com>
2023-02-05 23:08:22 +0530

    ODDPAIRS-codechef

---
+++ ODDPAIRS.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int T;
+    scanf("%d",&T);
+    while(T--){
+    long long int ix;
+    scanf("%lld",&ix);
+    printf("%lld\n",ix*ix/2);
+    }
+	return 0;
+}
+
